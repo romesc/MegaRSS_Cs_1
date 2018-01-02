afa4c5f280cf79cbc45b61b733491776299b8041
Rodrigo Mathias <ro_mescua@outlook.com>
2018-01-02 14:45:03 -0300

    Adição da classe de Dados

---
+++ MegaRSS_1/Classes/Dados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaRSS_1.Classes
+{
+    class Dados
+    {
+    }
+}
