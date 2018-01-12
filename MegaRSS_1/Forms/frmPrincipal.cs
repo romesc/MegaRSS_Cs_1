5b66505e93add3ce7912a49b136b203e45cf3ee4
Rodrigo Mathias <ro_mescua@outlook.com>
2018-01-12 13:34:14 -0300

    Adicionado borda redimensionável e movível.

--- MegaRSS_1/Forms/frmPrincipal.cs
+++ MegaRSS_1/Forms/frmPrincipal.cs
@@ -1,5 +1,6 @@
 ﻿using MegaRSS_1.Classes;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MegaRSS_1.Forms
@@ -9,6 +10,38 @@ namespace MegaRSS_1.Forms
         public frmPrincipal()
         {
             InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        private const int cGrip = 16;
+        private const int cCaption = 32;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rc = new Rectangle(this.ClientSize.Width - cGrip, this.ClientSize.Height - cGrip, cGrip, cGrip);
+            ControlPaint.DrawSizeGrip(e.Graphics, this.BackColor, rc);
+            rc = new Rectangle(0, 0, this.ClientSize.Width, cCaption);
+            e.Graphics.FillRectangle(Brushes.DarkBlue, rc);
         }
 
         private void btnCategorias_Click(object sender, EventArgs e)
